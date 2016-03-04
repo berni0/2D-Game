dac39637fd90ed78567a8a7f3fc4a312361cf31b
PlesMax <pless.max@gmail.com>
2016-03-04 10:16:33 +0100

    able to generate stages

--- 2D Game/src/game/utilities/RandomWorldCreator.java
+++ 2D Game/src/game/utilities/RandomWorldCreator.java
@@ -3,13 +3,19 @@ package game.utilities;
 public class RandomWorldCreator {
 	
 	//parameters
-	private static boolean generateGaps = true;
+	private static boolean generateGaps = false;	
+	private static boolean generateStages = true;	
 	
 	private static int gapProbability = 5; //between 0 and 100
+	private static int stageProbability = 5; //between 0 and 100
 	
 	private static int minGapLength = 3;
 	private static int maxGapLength = 7;
 	private static int solidAfterGap = 2;
+	
+	private static int minStageLength = 2;
+	private static int maxStageLength = 5;
+	private static int airAfterStage = 5;
 	
 	//working variables
 	private static int columnNo = 0;
@@ -54,24 +60,47 @@ public class RandomWorldCreator {
 	private static String generateTile(boolean isGround, int length) {
 		String s = "";
 		if(isGround) {
-			int p = gapProbability - (int) (Math.random() * 100);
-			if(p > 0 && generateGaps) {
-				int gapLength = (int) Math.round((maxGapLength - minGapLength) * Math.random()) + minGapLength;
-				for(int i = 1; i <= gapLength && columnNo <= length; i++) {
-					s += "0 ";
+			if(generateGaps) {
+				int p = gapProbability - (int) (Math.random() * 100);
+				if(p > 0) {
+					int gapLength = (int) Math.round((maxGapLength - minGapLength) * Math.random()) + minGapLength;
+					for(int i = 1; i <= gapLength && columnNo <= length; i++) {
+						s += "0 ";
+						columnNo++;
+					}
+					for(int j = 1; j <= solidAfterGap && columnNo <= length; j++) {
+						s += "1 ";
+						columnNo++;
+					}				
+				} else {
+					s = "1 ";
 					columnNo++;
 				}
-				for(int j = 1; j <= solidAfterGap && columnNo <= length; j++) {
-					s += "1 ";
-					columnNo++;
-				}				
 			} else {
 				s = "1 ";
 				columnNo++;
 			}
 		} else {
-			s= "0 ";
-			columnNo++;
+			if(generateStages) {
+				int p = stageProbability - (int) (Math.random() * 100);
+				if(p > 0) {
+					int stageLength = (int) Math.round((maxStageLength - minStageLength) * Math.random()) + minStageLength;
+					for(int i = 1; i <= stageLength && columnNo <= length; i++) {
+						s += "1 ";
+						columnNo++;
+					}
+					for(int j = 1; j <= airAfterStage && columnNo <= length; j++) {
+						s += "0 ";
+						columnNo++;
+					}				
+				} else {
+					s = "0 ";
+					columnNo++;
+				}
+			} else {
+				s = "0 ";
+				columnNo++;
+			}
 		}
 		return s;
 	}
